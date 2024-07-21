37e014316a836ccc5daec35134dad5c759334169
Matheus Todao <todaomatheus@hotmail.com>
2024-07-21 14:24:36 -0300

    feat: create a function to confirm account

---
+++ src/functions/accountConfirmation.ts
@@ -0,0 +1,24 @@
+import { bodyParser } from '@/utils/bodyParser';
+import { response } from '@/utils/response';
+import type { APIGatewayProxyEventV2 } from 'aws-lambda';
+import { ConfirmSignUpCommand } from '@aws-sdk/client-cognito-identity-provider';
+import { cognitoClient } from '@/libs/cognitoClient';
+
+export async function handler(event: APIGatewayProxyEventV2) {
+  try {
+    const body = bodyParser(event.body);
+    const command = new ConfirmSignUpCommand({
+      ClientId: process.env.COGNITO_CLIENT_ID,
+      Username: body.email,
+      ConfirmationCode: body.code,
+    });
+
+    await cognitoClient.send(command);
+
+    return response(204);
+  } catch (error) {
+    return response(500, {
+      message: 'Internal server error'
+    });
+  }
+}
