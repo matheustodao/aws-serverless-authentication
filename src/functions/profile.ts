d5cb73b85ba4fd33b14811e8fc62f37eb8339ce0
Matheus Todao <todaomatheus@hotmail.com>
2024-07-21 16:12:40 -0300

    feat: creates a function to get account information in cognito user pool

---
+++ src/functions/profile.ts
@@ -0,0 +1,31 @@
+import { cognitoClient } from '@/libs/cognitoClient';
+import { response } from '@/utils/response';
+import { AdminGetUserCommand } from '@aws-sdk/client-cognito-identity-provider';
+import { APIGatewayProxyEventV2WithJWTAuthorizer } from 'aws-lambda';
+
+export async function handler(event: APIGatewayProxyEventV2WithJWTAuthorizer) {
+  const userId = event.requestContext.authorizer.jwt.claims.sub as string;
+
+  const command = new AdminGetUserCommand({
+    Username: userId,
+    UserPoolId: process.env.COGNITO_POOL_ID
+  });
+
+  const { UserAttributes } = await cognitoClient.send(command);
+
+  const profile = UserAttributes?.reduce((profileObj, { Name, Value }) => {
+    const keyMap = {
+      sub: 'id',
+      given_name: 'name'
+    };
+
+    return {
+      ...profileObj,
+      [keyMap[Name as keyof typeof keyMap] ?? Name]: Value
+    };
+  }, {} as any);
+
+  return response(200, {
+    profile
+  });
+}
