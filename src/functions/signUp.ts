018b0a895299033689eddaab3e3949c720d06b16
Matheus Todao <todaomatheus@hotmail.com>
2024-07-21 14:11:06 -0300

    feat: creates function to sign up a user

---
+++ src/functions/signUp.ts
@@ -0,0 +1,36 @@
+import { bodyParser } from '@/utils/bodyParser';
+import { response } from '@/utils/response';
+import type { APIGatewayProxyEventV2 } from 'aws-lambda';
+import { SignUpCommand, UsernameExistsException } from '@aws-sdk/client-cognito-identity-provider';
+import { cognitoClient } from '@/libs/cognitoClient';
+
+export async function handler(event: APIGatewayProxyEventV2) {
+  try {
+    const body = bodyParser(event.body);
+    const command = new SignUpCommand({
+      ClientId: process.env.COGNITO_CLIENT_ID,
+      Username: body.email,
+      Password: body.password,
+      UserAttributes: [
+        { Name: 'given_name', Value: body.given_name }
+      ]
+    });
+
+    const { UserSub } = await cognitoClient.send(command);
+
+    return response(200, {
+      id: UserSub,
+      ...body
+    });
+  } catch (error) {
+    if (error instanceof UsernameExistsException) {
+      return response(401, {
+        message: 'This email already in use'
+      });
+    }
+
+    return response(500, {
+      message: 'Internal server error'
+    });
+  }
+}
